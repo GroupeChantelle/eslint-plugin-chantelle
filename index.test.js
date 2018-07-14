c2808c1066005de87e7e131bfad3f3f80e6f12b7
Furkan Tunalı <mail@furkantunali.com>
2018-07-14 23:09:57 +0200

    update tests

--- index.test.js
+++ index.test.js
@@ -1,15 +1,16 @@
 /* eslint fp/no-unused-expression:0, fp/no-nil:0, better/explicit-return:0 */
+import { configs, chantelle, legacy, chantelleCommonJs } from './src'
 
-import test from 'ava'
-import { configs, chantelle, legacy, chantelleCommonJs } from './'
+const checkArrayOfObjects = arrayOfObjects =>
+  arrayOfObjects.map(arrayOfObjects =>
+    expect(typeof arrayOfObjects).toBe('object')
+  )
 
-const checkArrayOfObjects = t => arrayOfObjects =>
-  arrayOfObjects.map(arrayOfObjects => t.is(typeof arrayOfObjects, 'object'))
+test('exports objects', () => {
+  checkArrayOfObjects([configs, chantelle, legacy, chantelleCommonJs])
+})
 
-test('exports objects', t =>
-  checkArrayOfObjects(t)([configs, chantelle, legacy, chantelleCommonJs]))
-
-test('exports objects 2', t => {
+test('exports objects 2', () => {
   ;(({ chantelle, legacy, chantelleCommonJs }) =>
-    checkArrayOfObjects(t)([chantelle, legacy, chantelleCommonJs]))(configs)
+    checkArrayOfObjects([chantelle, legacy, chantelleCommonJs]))(configs)
 })
